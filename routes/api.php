180998f47964269fc886ec71d5f99f93ba2637ef
Edgar Rosas <gary.blurk@gmail.com>
2020-03-09 09:53:36 -0700

    removed api closures

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +14,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', 'API\PatientController@store');
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
